ccc41ee660a5cfa8e4351613f4802719e12dddf2
Kevin <zhijunwang2002@gmail.com>
2023-01-23 02:13:11 -0500

    separate models

--- backend/app.py
+++ backend/app.py
@@ -3,64 +3,16 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from flask import Flask, abort, jsonify, request
+from config import configure_db, db
+from models import Formula
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:wzj20020407@localhost:5432/arist_lab"
-db = SQLAlchemy(app)
+configure_db(app)
 migrate = Migrate(app, db)
 
-class Formula(db.Model):
-    __tablename__ = 'formula'
-
-    id = db.Column(db.Integer, primary_key=True)
-
-    is_conclusion = db.Column(db.Boolean(), default=False)
-    is_binary = db.Column(db.Boolean(), nullable=True, default=False)
-    is_unary = db.Column(db.Boolean(), nullable=True, default=False)
-    is_function = db.Column(db.Boolean(), nullable=True, default=False)
-    is_variable = db.Column(db.Boolean(), nullable=True, default=False)
-    has_atom_inside = db.Column(db.Boolean(), nullable=True, default=False)
-
-    left = db.Column(db.BigInteger(), nullable=True)
-    right = db.Column(db.BigInteger(), nullable=True)
-    connective = db.Column(db.Integer(), nullable=True)
-
-    quantifier = db.Column(db.Integer(), nullable=True)
-    quantifier_variable = db.Column(db.BigInteger(), nullable=True)
-    negation = db.Column(db.Boolean(), nullable=True)
-
-    function_name = db.Column(db.String(), nullable=True)
-    variable_name = db.Column(db.String(), nullable=True)
-
-    inside = db.Column(db.BigInteger(), nullable=True)
-
-    def to_json(self):
-        return {
-            'id': self.id,
-            'is_conclusion': self.is_conclusion,
-            'is_binary': self.is_binary,
-            'is_unary': self.is_unary,
-            'is_function': self.is_function,
-            'is_variable': self.is_variable,
-            'has_atom_inside': self.has_atom_inside,
-            'left': self.left,
-            'right': self.right,
-            'connective': self.connective,
-            'quantifier': self.quantifier,
-            'quantifier_variable': self.quantifier_variable,
-            'negation': self.negation,
-            'function_name': self.function_name,
-            'variable_name': self.variable_name,
-            'inside': self.inside
-        }
-
-class Session(db.Model):
-    __tablename__ = "session"
-
-    id = db.Column(db.Integer, primary_key=True)
-    # formula = db.relationship(Formula)
 
 @app.route('/function-atom', methods=['POST'])
 def create_function():
@@ -99,5 +51,5 @@ def get_formulas():
     pass                                                                                                                                                   
 
 @app.route("/")
-def hello_world():
+def index():
     return "<p>Hello, World!</p>"
