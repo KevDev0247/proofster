27bc39a87d98eb97704e8cc3221db30787d90b8e
Kevin <zhijunwang2002@gmail.com>
2023-02-07 12:21:39 -0500

    function name

--- domain/transpiler/test.py
+++ domain/transpiler/test.py
@@ -2,7 +2,7 @@ import unittest
 from service import transpile
 
 class Test(unittest.TestCase):
-    def test_service(self):
+    def test_transpiler_service(self):
         input = "FORM F y FORM G y AND FORM F x FORM G x -> OR EXIST y FORALL x".split()
         expected = "∀∃((F(y) ∧ G(y)) ∨ (F(x) ⇒ G(x)))"
         actual = transpile(input).to_string()
