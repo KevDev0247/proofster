1e9056191ff85413524bfc87a887db97698b5643
Kevin <zhijunwang2002@gmail.com>
2023-02-10 00:31:44 -0500

    added prenex normalization service and its test

--- domain/prenex_normalizer/test.py
+++ domain/prenex_normalizer/test.py
@@ -1,9 +1,19 @@
 import unittest
 from factory import create_formula_from_json
 from test_input import input
+from service import Normalizer
 
 class Test(unittest.TestCase):
     def test_formula_factory(self):
         actual = create_formula_from_json(input).to_string()
         expected = "∀∃((F(y) ∧ G(y)) ∨ (F(x) ⇒ G(x)))"
+        self.assertEqual(actual, expected)
+
+    def test_prenex_normalizer_service(self):
+        formula = create_formula_from_json(input)
+        normalizer = Normalizer([formula])
+
+        normalizer.normalize_to_prenex()
+        actual = normalizer.to_string().pop()
+        expected = "∀x1((F(f(x1)) ∧ G(f(x1))) ∨ (¬F(x1) ∨ G(x1)))"
         self.assertEqual(actual, expected)
