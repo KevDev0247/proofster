5a5475c13f4eb53c199eddfb8512663873f12e70
Kevin <zhijunwang2002@gmail.com>
2023-03-01 00:36:48 -0500

    transpiler rename

--- domain/reverse_polish_transpiler/service.py
+++ domain/reverse_polish_transpiler/service.py
@@ -8,36 +8,36 @@ from models.Unary import Unary
 from models.Variable import Variable
 
 def transpile(tokens: List[str]) -> Formula:
-    formula_holder = []
+    stack = []
     var_count = {}
 
     for t, token in enumerate(tokens):
         if token == "->":
-            right = formula_holder.pop()
-            left = formula_holder.pop()
+            right = stack.pop()
+            left = stack.pop()
 
-            formula_holder.append(
+            stack.append(
                 Binary(left, right, Connective.IMPLICATION)
             )
         if token == "<->":
-            right = formula_holder.pop()
-            left = formula_holder.pop()
+            right = stack.pop()
+            left = stack.pop()
 
-            formula_holder.append(
+            stack.append(
                 Binary(left, right, Connective.BICONDITIONAL)
             )
         if token == "AND":
-            right = formula_holder.pop()
-            left = formula_holder.pop()
+            right = stack.pop()
+            left = stack.pop()
 
-            formula_holder.append(
+            stack.append(
                 Binary(left, right, Connective.AND)
             )
         if token == "OR":
-            right = formula_holder.pop()
-            left = formula_holder.pop()
+            right = stack.pop()
+            left = stack.pop()
 
-            formula_holder.append(
+            stack.append(
                 Binary(left, right, Connective.OR)
             )
         if token == "FORM":
@@ -49,39 +49,39 @@ def transpile(tokens: List[str]) -> Formula:
             else:
                 var_count[var_name] += 1
 
-            formula_holder.append(
+            stack.append(
                 Function(func_name, Variable(var_name))
             )
         if token == "NOT":
-            inside = formula_holder.pop()
+            inside = stack.pop()
 
-            formula_holder.append(
+            stack.append(
                 Unary(inside, Quantifier.NONE, True, "")
             )
         if token == "FORALL":
-            inside = formula_holder.pop()
+            inside = stack.pop()
             var_name = tokens[t + 1]
 
             if var_name not in var_count:
                 var_count[var_name] = 1
 
-            formula_holder.append(
+            stack.append(
                 Unary(inside, Quantifier.UNIVERSAL, False, var_name)
             )
         if token == "EXIST":
-            inside = formula_holder.pop()
+            inside = stack.pop()
             var_name = tokens[t + 1]
 
             if var_name not in var_count:
                 var_count[var_name] = 1
 
-            formula_holder.append(
+            stack.append(
                 Unary(inside, Quantifier.EXISTENTIAL, False, var_name)
             )
         if token == "done":
             break
 
-    formula = formula_holder.pop()
+    formula = stack.pop()
     formula.set_var_count(var_count)
     return formula
 
