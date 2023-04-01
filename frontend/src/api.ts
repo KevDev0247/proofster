c618beb9a0c824943f78b8e13098d910979bec90
Kevin <zhijunwang2002@gmail.com>
2023-04-01 18:26:02 -0400

    Add debug urls

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,8 +1,18 @@
 import axios from "axios";
 
-let baseWorkspaceURL = process.env.REACT_APP_WORKSPACES_SERVICE_ADDRESS + "/api/workspaces/";
-let baseFormulaURL = process.env.REACT_APP_FORMULAS_SERVICE_ADDRESS + "/api/formulas/";
-let baseAlgorithmURL = process.env.REACT_APP_ALGORITHM_SERVICE_ADDRESS + "/v1/normalize";
+const debug = true;
+
+let baseWorkspaceURL, baseFormulaURL, baseAlgorithmURL;
+
+if (debug) {
+  baseWorkspaceURL = process.env.REACT_APP_WORKSPACES_LOCAL + "/api/workspaces/";
+  baseFormulaURL = process.env.REACT_APP_FORMULAS_LOCAL + "/api/formulas/";
+  baseAlgorithmURL = process.env.REACT_APP_ALGORITHM_LOCAL + "/v1/normalize";
+} else {
+  baseWorkspaceURL = process.env.REACT_APP_WORKSPACES_SERVICE_ADDRESS + "/api/workspaces/";
+  baseFormulaURL = process.env.REACT_APP_FORMULAS_SERVICE_ADDRESS + "/api/formulas/";
+  baseAlgorithmURL = process.env.REACT_APP_ALGORITHM_SERVICE_ADDRESS + "/v1/normalize";
+}
 
 export const WORKSPACE_API = axios.create({
   baseURL: baseWorkspaceURL,
